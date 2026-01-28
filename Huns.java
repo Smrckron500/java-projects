5fe8e29904cb786e3b4ac3211b8573ae80c3e19e
Logan Smrcka <logansmrcka@gmail.com>
2026-01-27 20:29:05 -0600

    Using strings and ints for the first time
    not much, similar to superworl, but with strings and huns

---
+++ Huns.java
@@ -0,0 +1,11 @@
+public class Huns {
+    public static void main(String[] args) {
+        int Hi = 49;
+        String Huns = "The Huns are coming and theres";
+        String Huns92 = "of them";
+        System.out.println(Huns);
+        System.out.println(Hi);
+        System.out.println(Huns92);
+    }
+
+}
